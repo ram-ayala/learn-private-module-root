af72be919254037fe2da97d51aaeb22c57a8f0d6
ram-ayala <74069245+ram-ayala@users.noreply.github.com>
2023-10-26 18:52:54 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "s3-webapp" {
-  source  = "app.terraform.io/hashicorp-learn/s3-webapp/aws"
+  source  = "app.terraform.io/ramairlines408/module-aws-s3-webapp/private"
   name    = var.name
   region  = var.region
   prefix  = var.prefix
